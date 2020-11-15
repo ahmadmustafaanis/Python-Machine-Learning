b29795705527d1364b4cb71dac3d3a0851b843f2
ahmadmustafaanis <ahmadanis5050@gmail.com>
2020-11-15 11:07:00 +0500

    Decesion Region Indentation Errors

--- Dimensionality Reduction/plot_decesion_region.py
+++ Dimensionality Reduction/plot_decesion_region.py
@@ -12,15 +12,15 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
 
-   Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()].T))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()].T))
 
-   Z = Z.reshape(xx1.shape)
+    Z = Z.reshape(xx1.shape)
 
-   plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-   plt.xlim(xx1.min(), xx1.max())
-   plt.ylim(xx2.min(), xx2.max())
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
 
-   for idx, cl in enumerate(np.unique(y)):
+    for idx, cl in enumerate(np.unique(y)):
        plt.scatter(x=X[y==cl,0],
                    y=X[y==cl,1],
                    alpha=0.6,
