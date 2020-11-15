6bc068db4fff3e0e8aa68425c78500bca4937ea2
ahmadmustafaanis <ahmadanis5050@gmail.com>
2020-11-15 12:15:36 +0500

    plotting decesion region

--- Dimensionality Reduction/plot_decesion_region.py
+++ Dimensionality Reduction/plot_decesion_region.py
@@ -1,5 +1,6 @@
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
     markers = ('s','x','o','^','v')
@@ -7,12 +8,12 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     x1_min, x1_max = X[:,0].min() -1 , X[:,0].max() +1
-    x2_min, x2_max = X[:, 2].min()-1 , X[:, 1].max()+1
+    x2_min, x2_max = X[:, 1].min()-1 , X[:, 1].max()+1
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
 
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()].T))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
 
     Z = Z.reshape(xx1.shape)
 
