e9c71d271566085d55d1852722613db54a7053af
ahmadmustafaanis <ahmadanis5050@gmail.com>
2020-11-20 02:44:22 +0500

    LDA with sklearn

--- Dimensionality Reduction/Linear_Discrminant_analysis_sklearn.py
+++ Dimensionality Reduction/Linear_Discrminant_analysis_sklearn.py
@@ -1,8 +1,10 @@
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-
+from sklearn.linear_model import LogisticRegression
+from plot_decesion_region import plot_decision_regions
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 '''
@@ -22,3 +24,31 @@ X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
 
 
+
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+X_test_lda = lda.transform(X_test_std)
+
+
+lr = LogisticRegression(multi_class='ovr', random_state=1, solver = 'lbfgs')
+
+
+lr.fit(X_train_lda[0:2,:], y_train[0:2])
+
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.title("Before Fitting Model")
+plt.xlabel("LD 1")
+plt.ylabel("LD 2")
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.title("After Fitting Model")
+plt.xlabel("LD 1")
+plt.ylabel("LD 2")
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
