eba768042df61b824ad906dfa2078afaeafa392e
ahmadmustafaanis <ahmed.anis@redbuffer.net>
2022-05-07 15:31:32 +0500

    formatting

--- Adaline.py
+++ Adaline.py
@@ -1,5 +1,7 @@
 import numpy as np
-class Adaline():
+
+
+class Adaline:
     """
         Implements Adaptive Linear Neuron Classifier
     Parameters:
@@ -9,43 +11,43 @@ class Adaline():
             Epochs
         random_state: int
             Randome number generator for weight initlaization
-    
+
     Attributes:
         w_ : 1d array
             1d Array containing weights
         cost_ : list
             List containg cost for each epoch
-    
+
     """
 
-    def __init__ (self, eta=0.01, n_iter=50, random_state =1):
-        self.eta = eta 
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def net_input(self, X):
-        return np.dot(X, self.w_[1,:]) + self.w_[0,:]
+        return np.dot(X, self.w_[1, :]) + self.w_[0, :]
 
     def activation(self, X):
         return X
 
     def fit(self, X, y):
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=1.0, size=1+X.shape[1])
+        self.w_ = rgen.normal(loc=0.0, scale=1.0, size=1 + X.shape[1])
 
         self.cost_ = []
 
         for _ in range(self.n_iter):
-            net_input = self.net_input(X)
+            net_input = self.net_input(X)  # hp = wx + b
             output = self.activation(net_input)
 
             errors = y - output
-            self.w_[1,:] += X.T.dot(errors)
-            self.w_[0,:] += errors.sum()  
-            cost = (errors**2).sum()/2 
+            self.w_[1, :] += X.T.dot(errors)
+            self.w_[0, :] += errors.sum()
+            cost = (errors**2).sum() / 2
             self.cost_.append(cost)
 
-        return self 
+        return self
 
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
--- KNN.py
+++ KNN.py
@@ -5,22 +5,25 @@ from decesion_boundary import plot_decision_region
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-iris = load_iris()
-X = iris['data'][:, [2,3]]
-y = iris['target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 1, stratify =y)
+iris = load_iris()
+X = iris["data"][:, [2, 3]]
+y = iris["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y
+)
 
 
 X_comb = np.vstack((X_train, X_test))
 y_comb = np.hstack((y_train, y_test))
 
 
-knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric="minkowski")
 knn.fit(X_train, y_train)
 
-plot_decision_region(X_comb, y_comb, knn, test_idx=range(105,150))
-plt.xlabel('Petal Length')
-plt.ylabel('Petal Width')
+plot_decision_region(X_comb, y_comb, knn, test_idx=range(105, 150))
+# plt.xlabel('Petal Length')
+# plt.ylabel('Petal Width')
 plt.tight_layout()
 plt.show()
